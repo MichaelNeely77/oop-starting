12b53614ce3bdb550eee2da8b1507628525fc88a
Michael Neely <michael@michaelneely.net>
2023-05-29 12:10:33 -0400

    Built basic product list and products

---
+++ assets/scripts/app.js
@@ -0,0 +1,40 @@
+const productList = {
+    products: [
+        { title: 'A Pillow',
+            imageUrl: 'https://cdn.sleepnumber.com/image/upload/f_auto,q_auto:eco/v1666283228/workarea/catalog/product_images/pillow-pcp/Pillow-PCP_PDP_Postcard_Variant_classic',
+            price: 19.99,
+            description: 'A soft pilllow'
+        },
+        { title: 'A carpet',
+            imageUrl: 'https://secure.img1-fg.wfcdn.com/im/09651980/resize-h445%5Ecompr-r85/1922/192265957/Nisar+Performance+Beige%2FYellow+Machine+Washable+Rug.jpg',
+            price: 179.99,
+            description: 'A soft carpet'
+        }
+    ],
+    render() {
+        const renderHook = document.getElementById('app');
+        const prodList = document.createElement('ul');
+        prodList.className = 'product-list';
+        for (const prod of this.products) {
+            const prodEl = document.createElement('li');
+            prodEl.className = 'product-item';
+            prodEl.innerHTML = `
+                <div>
+                    <img src="${prod.imageUrl}" alt="${prod.title}">
+                    <div class="product-item__content">
+                        <h2>${prod.title}</h2>
+                        <h3>\$${prod.price}</h3>
+                        <p>${prod.description}</p>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
+            `;
+
+            prodList.append(prodEl);
+        }
+
+        renderHook.append(prodList);
+    }
+};
+
+productList.render();
